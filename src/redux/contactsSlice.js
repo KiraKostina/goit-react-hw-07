98178636780d3c18812fbc6a3591959520ae3e30
KiraKostina <kostina.kira@gmail.com>
2024-06-06 00:33:16 +0300

    fix id for delete

--- src/redux/contactsSlice.js
+++ src/redux/contactsSlice.js
@@ -35,7 +35,7 @@ const contactsSlice = createSlice({
       .addCase(deleteContact.pending, handlePending)
       .addCase(deleteContact.fulfilled, (state, action) => {
         const index = state.items.findIndex(
-          contact => contact.id === action.payload
+          contact => contact.id === action.payload.id
         );
         state.items.splice(index, 1);
         state.loading = false;
